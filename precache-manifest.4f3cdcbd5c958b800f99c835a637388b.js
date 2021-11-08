decc8385cf91e7a238e5d0cce73501ad987d025c
celtdev <celtdev@gmail.com>
2021-11-08 02:59:20 -0800

    init

---
+++ precache-manifest.4f3cdcbd5c958b800f99c835a637388b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "260d287add8a6812e94c",
+    "url": "/static/css/main.d5db63a5.chunk.css"
+  },
+  {
+    "revision": "260d287add8a6812e94c",
+    "url": "/static/js/main.260d287a.chunk.js"
+  },
+  {
+    "revision": "aca7b42372bd08e9824e",
+    "url": "/static/js/runtime~main.aca7b423.js"
+  },
+  {
+    "revision": "13924fad9fa7b6319f41",
+    "url": "/static/css/2.994106a5.chunk.css"
+  },
+  {
+    "revision": "13924fad9fa7b6319f41",
+    "url": "/static/js/2.13924fad.chunk.js"
+  },
+  {
+    "revision": "9598f5c2d3b1c004d1a5ed83f5a48ac5",
+    "url": "/static/media/iconFrame.9598f5c2.svg"
+  },
+  {
+    "revision": "59b679e5566b8c75d24cd02a6a238bfa",
+    "url": "/static/media/boxBgMobile.59b679e5.png"
+  },
+  {
+    "revision": "9e1629d0a0662e443f11ddba821a65be",
+    "url": "/static/media/modalBg.9e1629d0.png"
+  },
+  {
+    "revision": "ebe5d0761695230a6d61e40224e3e2ba",
+    "url": "/index.html"
+  }
+];
